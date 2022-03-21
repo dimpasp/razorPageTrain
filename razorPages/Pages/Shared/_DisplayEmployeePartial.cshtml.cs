7aa858caa1fbf687532fb7deeba6a2a60f51e595
dimpasp <dim93pas@gmail.com>
2022-03-21 10:58:02 +0200

    add partial view

---
+++ razorPages/Pages/Shared/_DisplayEmployeePartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorPages.Pages.Shared
+{
+    public class _DisplayEmployeePartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
