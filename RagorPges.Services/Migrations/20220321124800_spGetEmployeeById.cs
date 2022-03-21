ed164fea6d2704c122608b5dfe439fa00ede2b62
dimpasp <dim93pas@gmail.com>
2022-03-21 15:00:35 +0200

    use stored procedure

---
+++ RagorPges.Services/Migrations/20220321124800_spGetEmployeeById.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPages.Services.Migrations
+{
+    public partial class spGetEmployeeById : Migration
+    {
+                    //Using stored procedure
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create Procedure spGetEmployeeById
+                            @Id int
+                            as
+                            Begin
+                             Select * from Employees
+                             Where Id = @Id
+                            End";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Drop procedure spGetEmployeeById";
+            migrationBuilder.Sql(procedure);
+
+        }
+    }
+}
